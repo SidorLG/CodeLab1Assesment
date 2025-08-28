d9fca846fed3d042a18590c8f45f6e472cbd9959
SidorLG <sidorlucian@yahoo.com>
2025-08-28 09:48:26 +0100

    Update Exercise1_Codeiscool.cpp

--- Exercise1_Codeiscool.cpp
+++ Exercise1_Codeiscool.cpp
@@ -3,5 +3,5 @@ using namespace std; // Lets us write standard commands like cout without extra 
 
 int main() { // Starting point of the program
     cout << "Code is cool" << endl; // Prints a message to the screen and moves to the next line
-    return 0; // Signals that the program finished running with success
+    return 0; //  End the program successfully 
 }
